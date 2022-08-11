2519543dd8d3c14e1caf315896d1f42a48a80883
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-08-11 20:36:23 +0200

    Create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { FormComponent } from './components/form/form.component';
+import { PopupComponent } from './components/popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    UserListComponent,
+    UserCardComponent,
+    FormComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,

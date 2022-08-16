42bc91e907389140b49dca214eac0b4751241382
Francisco Sánchez <sanchezgarridof@gmail.com>
2022-08-16 12:23:15 +0200

    cretate ruoutes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './components/form/form.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { UserDetailComponent } from './components/user-detail/user-detail.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: UserListComponent },
+  { path: 'user/:id', component: UserDetailComponent },
+  { path: 'newuser', component: FormComponent },
+  { path: 'updateuser/:id', component: FormComponent },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
